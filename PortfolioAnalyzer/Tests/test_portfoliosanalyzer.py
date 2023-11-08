fa6f20400720834979a512a362c21bdc50e2e225
joperezdelcerro <jperez@qontigo.com>
2023-11-08 16:57:18 -0300

    Cleaning up to do the exercise at the training

--- PortfolioAnalyzer/Tests/test_portfoliosanalyzer.py
+++ PortfolioAnalyzer/Tests/test_portfoliosanalyzer.py
@@ -4,28 +4,26 @@ from PortfolioAnalyzer.portfolioanalyzer import PortfoliosAnalyzer
 
 
 class TestPortfolioAnalyzer:
-    def setup_method(self):
-        self.portfolio_2021 = "PortfolioAnalyzer/Tests/Data/portfolio_2021.csv"
-        self.portfolio_2022 = "PortfolioAnalyzer/Tests/Data/portfolio_2022.csv"
-
     def test_count_lost_bonds_between_portfolios(self):
         # GIVEN
-        pa = PortfoliosAnalyzer(self.portfolio_2021, self.portfolio_2022)
+        portfolio_2021 = "PortfolioAnalyzer/Tests/Data/portfolio_2021.csv"
+        portfolio_2022 = "PortfolioAnalyzer/Tests/Data/portfolio_2022.csv"
+        pa = PortfoliosAnalyzer(portfolio_2021, portfolio_2022)
 
         # WHEN
         actual_number_of_bonds = pa.count_lost_bonds()
 
         # THEN
-        expected_number_of_bonds = 2
-        assert expected_number_of_bonds == actual_number_of_bonds, "Should be 2 bonds"
+        assert False
 
     def test_find_max_increased_bond(self):
         # GIVEN
-        pa = PortfoliosAnalyzer(self.portfolio_2021, self.portfolio_2022)
+        portfolio_2021 = "PortfolioAnalyzer/Tests/Data/portfolio_2021.csv"
+        portfolio_2022 = "PortfolioAnalyzer/Tests/Data/portfolio_2022.csv"
+        pa = PortfoliosAnalyzer(portfolio_2021, portfolio_2022)
 
         # WHEN
-        actual_most_increased_bond = pa.find_max_increased_bond()
+
 
         # THEN
-        expected_most_increased_bond = "BondB"
-        assert expected_most_increased_bond == actual_most_increased_bond, "Should be BondB"
+        assert False
