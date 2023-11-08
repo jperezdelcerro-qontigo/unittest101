aeae3d0983b7391d1fc02a0fc9121246a471cc64
joperezdelcerro <jperez@qontigo.com>
2023-11-08 16:25:23 -0300

    Cleaning up branch, improving tests

--- PortfolioAnalyzer/Tests/test_portfoliosanalyzer.py
+++ PortfolioAnalyzer/Tests/test_portfoliosanalyzer.py
@@ -1,44 +1,31 @@
-import unittest
 from io import StringIO
 
 from PortfolioAnalyzer.portfolioanalyzer import PortfoliosAnalyzer
 
 
-class TestPortfolioAnalyzer(unittest.TestCase):
-    def setUp(self):
-        self.portfolio_2021 = StringIO("""InstrumentId,Name,Date,Price
-                                    A123,BondA,2021-10-29,100.0
-                                    A456,BondB,2021-10-29,200.0
-                                    A456,BondD,2021-10-29,200.0
-                                    A789,BondC,2021-10-29,300.1""")
-
-        self.portfolio_2022 = StringIO("""InstrumentId,Name,Date,Price
-                                    A123,BondE,2022-10-29,100.0
-                                    A456,BondB,2022-10-29,200.5
-                                    A789,BondC,2022-10-29,300.2""")
+class TestPortfolioAnalyzer:
+    def setup_method(self):
+        self.portfolio_2021 = "PortfolioAnalyzer/Tests/Data/portfolio_2021.csv"
+        self.portfolio_2022 = "PortfolioAnalyzer/Tests/Data/portfolio_2022.csv"
 
     def test_count_lost_bonds_between_portfolios(self):
         # GIVEN
-        fip = PortfoliosAnalyzer(self.portfolio_2021, self.portfolio_2022)
+        pa = PortfoliosAnalyzer(self.portfolio_2021, self.portfolio_2022)
 
         # WHEN
-        actual_number_of_bonds = fip.count_lost_bonds()
+        actual_number_of_bonds = pa.count_lost_bonds()
 
         # THEN
         expected_number_of_bonds = 2
-        self.assertEqual(expected_number_of_bonds, actual_number_of_bonds, msg="Should be 2 bonds")
+        assert expected_number_of_bonds == actual_number_of_bonds, "Should be 2 bonds"
 
     def test_find_max_increased_bond(self):
         # GIVEN
-        fip = PortfoliosAnalyzer(self.portfolio_2021, self.portfolio_2022)
+        pa = PortfoliosAnalyzer(self.portfolio_2021, self.portfolio_2022)
 
         # WHEN
-        actual_most_increased_bond = fip.find_max_increased_bond()
+        actual_most_increased_bond = pa.find_max_increased_bond()
 
         # THEN
         expected_most_increased_bond = "BondB"
-        self.assertEqual(expected_most_increased_bond, actual_most_increased_bond, msg="Should be BondB")
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert expected_most_increased_bond == actual_most_increased_bond, "Should be BondB"
